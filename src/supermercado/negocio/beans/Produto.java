bd70fa507626191f285ee3124cba3c5d26f9cc46
Letícia Gomes <leticia.gomess@ufrpe.br>
2024-01-18 18:45:54 -0300

    Update Produto.java

--- src/supermercado/negocio/beans/Produto.java
+++ src/supermercado/negocio/beans/Produto.java
@@ -1,4 +1,35 @@
 package supermercado.negocio.beans;
 
 public class Produto {
+    private String nomeProd;
+    private static int codigoProd = 0;
+    private String categoriaProd;
+    private float valorProd;
+
+    public Produto(String nomeProd, String categoriaProd, float valorProd){
+        Produto.codigoProd++;
+        this.nomeProd = nomeProd;
+        this.categoriaProd = categoriaProd;
+        this.valorProd = valorProd;
+    }
+
+    public float getValorProd() {
+        return valorProd;
+    }
+
+    public static int getCodigoProd() {
+        return codigoProd;
+    }
+
+    public String getCategoriaProd() {
+        return categoriaProd;
+    }
+
+    public String getNomeProd() {
+        return nomeProd;
+    }
+
+    public void setValorProd(float valorProd) {
+        this.valorProd = valorProd;
+    }
 }
