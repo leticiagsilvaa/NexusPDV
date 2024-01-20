73d997796b3b7fb18ddbd107c5ecd705fde79b7e
Malu Vasconcelos <96549606+maluvasc@users.noreply.github.com>
2024-01-20 16:14:50 -0300

    Main changes to Repositorio testing

--- src/supermercado/Main.java
+++ src/supermercado/Main.java
@@ -12,10 +12,10 @@ public class Main {
     public static void main(String args[]) {
         //INSTANCIAÇÕES
         Login login = new Login("Luffy", "12345678");
-        Funcionario funcionario = new Funcionario(login, "Maria", "1234");
-        Funcionario funcionario1 = new Funcionario(login, "Maria" ,"1233445");
-        Funcionario funcionario2 = new Funcionario(login, "José" ,"1234555");
-        Funcionario funcionario3 = new Funcionario(login, "Catarina" ,"1245");
+        Funcionario funcionario = new Funcionario(login, "José" ,"1234555");
+        Funcionario funcionario1 = new Funcionario(login, "Catarina" ,"1245");
+        Funcionario funcionario2 = new Funcionario(login, "Maria" ,"1233445");
+        Funcionario funcionario3 = new Funcionario(login, "Maria", "1234");
         Produto produto = new Produto("Café", "Bebida", 2.50F);
         Produto produto2 = new Produto("Arroz", "Comida", 3.50F);
 
@@ -26,12 +26,17 @@ public class Main {
         repo.add(funcionario1);
         repo.add(funcionario2);
         repo.add(funcionario3);
+        System.out.println("TODOS: ");
         System.out.println(Arrays.toString(repo.getAll()));
         repo.delete(1);
+        System.out.println("TODOS MENOS DELETADO: ");
         System.out.println(Arrays.toString(repo.getAll()));
+        System.out.println("FUNCIONARIO COD 2: ");
         System.out.println(repo.getOne(2));
-        repo.findByName("Maria");
+        System.out.println("FUNCIONARIOS CHAMADOS MARIA: ");
+        System.out.println(Arrays.toString(repo.findByName("Maria")));
         System.out.println("DIGITE NOME E DEPOIS CPF PARA ATUALIZAÇÃO: ");
         repo.update(3);
+        System.out.println("TODOS: ");
         System.out.println(Arrays.toString(repo.getAll()));
 }}
