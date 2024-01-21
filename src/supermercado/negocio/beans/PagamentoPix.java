3a6df0bb2bca22a2823c4e08d021f63d218d3b77
RuanRodrigues1 <142950355+RuanRodrigues1@users.noreply.github.com>
2024-01-21 12:56:24 -0300

    Update PagamentoPix.java

--- src/supermercado/negocio/beans/PagamentoPix.java
+++ src/supermercado/negocio/beans/PagamentoPix.java
@@ -1,6 +1,27 @@
 package supermercado.negocio.beans;
 
-public class PagamentoPix extends Pagamento{
-    public void pagar(){}
+public class PagamentoPix extends MinhaClasse {
+    private String codigoPix;
+
+    public PagamentoPix(OrderStatus status, int id, LocalDateTime data, Venda venda, Double valor, String codigoPix) {
+        super(status, id, data, venda, valor);
+        this.codigoPix = codigoPix;
+    }
+
+    public String getCodigoPix() {
+        return codigoPix;
+    }
+
+    public void setCodigoPix(String codigoPix) throws PagamentoPixException {
+        if (codigoPix == null || codigoPix.isEmpty()) {
+            throw new PagamentoPixException("O código Pix não pode ser nulo.");
+        }
+        this.codigoPix = codigoPix;
+    }
+
+    @Override
+    public void pagar() throws Pagamento {
+        
+        setStatus(OrderStatus.EFETUADO);
 
 }
