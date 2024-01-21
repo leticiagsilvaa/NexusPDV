d647b2c78feea3ebdd2e2c03f2967bcb8874b402
RuanRodrigues1 <142950355+RuanRodrigues1@users.noreply.github.com>
2024-01-21 12:49:41 -0300

    Update PagamentoCartao.java

--- src/supermercado/negocio/beans/PagamentoCartao.java
+++ src/supermercado/negocio/beans/PagamentoCartao.java
@@ -1,6 +1,35 @@
 package supermercado.negocio.beans;
 
+import java.time.LocalDateTime;
+
 public class PagamentoCartao extends Pagamento{
-    public void pagar(){}
+    private String numeroCartao;
+
+    public PagamentoCartao(OrderStatus status, int id, LocalDateTime data, Venda venda, Double valor, String numeroCartao) {
+        super(status, id, data, venda, valor);
+        this.numeroCartao = numeroCartao;
+    }
+
+    public String getNumeroCartao() {
+        return numeroCartao;
+    }
+
+    public void setNumeroCartao(String numeroCartao) throws PagamentoCartaoException {
+        if (numeroCartao == null || numeroCartao.isEmpty()) {
+            throw new PagamentoCartaoException("O número do cartão não pode estar vazio.");
+        }
+        this.numeroCartao = numeroCartao;
+    }
+
+    @Override
+    public void pagar() {
+        
+        System.out.println("Pagamento com cartão realizado. Número do cartão: " + numeroCartao);
+        setStatus(OrderStatus.ENTREGUE);  
+    }
+}
+
+        
+    }
 
 }
