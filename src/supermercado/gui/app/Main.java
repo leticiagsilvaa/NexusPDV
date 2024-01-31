ebd2b192c07c67ea0e46c6b477ee875c89bf7e83
Maria Luisa Vasconcelos <96549606+maluvasc@users.noreply.github.com>
2024-01-31 14:58:56 -0300

    app executable

---
+++ src/supermercado/gui/app/Main.java
@@ -0,0 +1,30 @@
+package supermercado.gui.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage){
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("../telas/login.fxml"));
+            Scene scene = new Scene(parent);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/supermercado/gui/telas/CadastrarFuncionariosController.java
@@ -0,0 +1,13 @@
+package supermercado.gui.telas;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class CadastrarFuncionariosController {
+
+    @FXML
+    private Button bt;
+
+}
