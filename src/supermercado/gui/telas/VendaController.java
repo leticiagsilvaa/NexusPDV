ab6a36c613aa49391427510569b45559f76a643e
Maria Luisa Vasconcelos <96549606+maluvasc@users.noreply.github.com>
2024-02-05 08:41:11 -0300

    button exit in venda

--- src/supermercado/gui/telas/VendaController.java
+++ src/supermercado/gui/telas/VendaController.java
@@ -1,4 +1,18 @@
 package supermercado.gui.telas;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+
 public class VendaController {
+
+    @FXML
+    private Button bt;
+
+    public void onBtAction(javafx.event.ActionEvent actionEvent) {
+        Stage stage = (Stage) bt.getScene().getWindow();
+        stage.close();
+    }
 }
