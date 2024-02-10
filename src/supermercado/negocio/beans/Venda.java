cd0f9c096b92721d983d7b79a25259dc84760012
RuanRodrigues1 <142950355+RuanRodrigues1@users.noreply.github.com>
2024-02-10 11:31:59 -0300

    Update Venda.java

--- src/supermercado/negocio/beans/Venda.java
+++ src/supermercado/negocio/beans/Venda.java
@@ -2,7 +2,7 @@ package supermercado.negocio.beans;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import supermercado.negocio.exceptions.PagamentoException;
 public class Venda {
 
     private final int idVenda;
@@ -14,32 +14,34 @@ public class Venda {
     private int idCaixa;
     private Funcionario funcionario;
 
-    public Venda(int idCaixa, String loginFuncionario){
+    public Venda(int idCaixa, Funcionario funcionario) {
         Venda.totalIdVenda++;
-        this.idVenda =  totalIdVenda;
+        this.idVenda = totalIdVenda;
         this.idCaixa = idCaixa;
+        this.funcionario = funcionario;
         this.listaItens = new ArrayList<>();
-    }
-
-    public Funcionario getFuncionario() {
-        return funcionario;
     }
 
     public int getIdVenda() {
         return idVenda;
     }
 
-    public List<Produto> getListaItens(){
-        return this.listaItens;
-    }
-    public int getIdCaixa(){
-        return this.idCaixa;
+    public List<Produto> getListaItens() {
+        return listaItens;
     }
 
-    public Double getSubtotal(){
-        return this.subtotal;
+    public int getIdCaixa() {
+        return idCaixa;
     }
 
+    public Double getSubtotal() {
+        return subtotal;
+    }
+
+    public Double calcularTotal() {
+        // Descontos ou outros cálculos podem ser adicionados aqui
+        return subtotal;
+    }
 
     public void adicionarItemLista(Produto produto, int quantidade) {
         Item item = new Item(produto, quantidade);
@@ -56,17 +58,28 @@ public class Venda {
         }
     }
 
-
-    public Double calcularTotal() {
-        // descontos???
-        return subtotal;
-    }
-
     public Double getTroco() {
         return this.troco;
     }
 
+    public void efetuarPagamento(Pagamento pagamento) throws PagamentoException {
+        if (pagamento == null) {
+            throw new PagamentoException("Forma de pagamento inválida.");
+        }
 
-    // falta finalizar e calcular troco
+        //Realizar o pagamento.
+        pagamento.pagar();
 
+        //Calcular o troco, e se for pagamento em dinheiro:
+        if (pagamento instanceof PagamentoDinheiro) {
+            Double total = calcularTotal();
+            PagamentoDinheiro pagamentoDinheiro = (PagamentoDinheiro) pagamento;
+            Double valorPago = total + pagamentoDinheiro.getTroco();
+            if (valorPago >= total) {
+                this.troco = valorPago - total;
+            } else {
+                throw new PagamentoException("Valor pago insuficiente.");
+            }
+        }
+    }
 }
